e381367c0c18dae6095cf8e7a087061f1d955d13
DIL JAN ALI <86557442+DILJAN786@users.noreply.github.com>
2023-03-28 11:14:01 +0500

    Update callback-fucntion.js

--- callback-fucntion.js
+++ callback-fucntion.js
@@ -1,14 +1,14 @@
 console.log("call back fucntion");
 
-// function hello(){
-//     console.log("hello from first fucntion");
-// }
+function hello(){
+    console.log("hello from first fucntion");
+}
 
-// function hello2(a){
-// console.log("hello this is second function");
-// a();
-// }
-// hello2(hello);
+function hello2(a){
+console.log("hello this is second function");
+a();
+}
+hello2(hello);
 
 function func(b){
     console.log("hello from first fucn");
@@ -17,7 +17,7 @@ function func(b){
 
 function myfunc(a){
     console.log("this is my second func");
-    // console.log(a)
+    console.log(a)
     a("hi");
 }
 myfunc(func);
