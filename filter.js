7d79ddced323e9e9f05194bf301aa8731113712b
DIL JAN ALI <86557442+DILJAN786@users.noreply.github.com>
2023-04-01 14:02:37 +0500

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,11 @@
+let a = [2,3,4,5,6,7,8,9,10]
+
+// const myfunc = function(num){
+//     return num%2===0 ;
+// }
+const b = a.filter(function(num){
+    return num%2===0;
+});
+console.log(b);
+
+
